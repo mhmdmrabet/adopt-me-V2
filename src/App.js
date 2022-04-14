c576920631ac61f57c4c4e6a18cff006c1584725
mhmdmrabet <mhmdmrabet@yahoo.fr>
2022-04-14 21:56:30 +0200

    JSX & Config Eslint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "brand" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+    </div>
+  );
 };
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
