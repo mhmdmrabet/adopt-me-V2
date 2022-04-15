3f71259471288209e0f53f747f444e5fe3499fb1
mhmdmrabet <mhmdmrabet@yahoo.fr>
2022-04-15 15:42:38 +0200

    Search Form & Usetate Hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <SearchParams />
     </div>
   );
 };
