c15419feb4a5d4d73f14f20783bce1598323d5b6
mhmdmrabet <mhmdmrabet@yahoo.fr>
2022-04-15 16:01:04 +0200

    Mappind Data to Component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,10 @@
 import { useState } from "react";
-
+const ANIMALS = ["bird", "cat", "dog", "reptile", "rabbit"];
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = ["poodle", "bichon"];
   return (
     <div className="search-params">
       <form>
@@ -13,6 +16,48 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(event) => setLocation(event.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(event) => {
+              setAnimal(event.target.value);
+              setBreed("");
+            }}
+            onBlur={(event) => {
+              setAnimal(event.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="animal">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(event) => {
+              setBreed(event.target.value);
+            }}
+            onBlur={(event) => {
+              setBreed(event.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
